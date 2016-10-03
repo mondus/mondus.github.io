bba5ba8c847e6cda150ad000c25d3ebdcd76989f
mondus <p.richmond@sheffield.ac.uk>
2016-10-03 22:49:38 +0100

    Update analytics_downloads.js

--- assets/js/analytics_downloads.js
+++ assets/js/analytics_downloads.js
@@ -9,7 +9,8 @@ if (typeof jQuery != 'undefined') {
             if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
                 jQuery(this).click(function() {
                     var extLink = href.replace(/^https?\:\/\//i, '');
-                    ga.push(['_trackEvent', 'External', 'Click', extLink]);
+                    ga('send', 'event', 'Document', 'Download')
+                    // ga.push(['_trackEvent', 'External', 'Click', extLink]);
                     if (jQuery(this).attr('target') != undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
                         setTimeout(function() { location.href = href; }, 200);
                         return false;
@@ -19,14 +20,16 @@ if (typeof jQuery != 'undefined') {
             else if (href && href.match(/^mailto\:/i)) {
                 jQuery(this).click(function() {
                     var mailLink = href.replace(/^mailto\:/i, '');
-                    ga.push(['_trackEvent', 'Email', 'Click', mailLink]);
+                    //ga.push(['_trackEvent', 'Email', 'Click', mailLink]);
+                    ga('send', 'event', 'Email', 'Click')
                 });
             }
             else if (href && href.match(filetypes)) {
                 jQuery(this).click(function() {
                     var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
                     var filePath = href;
-                    ga.push(['_trackEvent', 'Download', 'Click-' + extension, filePath]);
+                    ga('send', 'event', 'LocalDocument', 'Download')
+                    //ga.push(['_trackEvent', 'Download', 'Click-' + extension, filePath]);
                     if (jQuery(this).attr('target') != undefined && jQuery(this).attr('target').toLowerCase() != '_blank') {
                         setTimeout(function() { location.href = baseHref + href; }, 200);
                         return false;
